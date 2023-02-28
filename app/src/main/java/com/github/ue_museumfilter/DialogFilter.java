ce45467cc0dd24021bbcf80f8c7ef86229b940cb
alexeses <46347629+alexeses@users.noreply.github.com>
2023-02-28 22:09:12 +0100

    added api response, stable

---
+++ app/src/main/java/com/github/ue_museumfilter/DialogFilter.java
@@ -0,0 +1,60 @@
+package com.github.ue_museumfilter;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class DialogFilter extends DialogFragment {
+    private Spinner filter_spinner;
+    private OnFilterSelectedListener listener;
+
+    public interface OnFilterSelectedListener {
+        void onFilterSelected(String distrito);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            listener = (OnFilterSelectedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context + " must implement OnFilterSelectedListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_filter, null);
+
+        filter_spinner = view.findViewById(R.id.filter_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.distritos_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        filter_spinner.setAdapter(adapter);
+
+        // TODO: Style the dialog to match the app theme
+
+        builder.setView(view)
+                .setPositiveButton("Filter", (dialog, which) -> {
+                    listener.onFilterSelected(filter_spinner.getSelectedItem().toString());
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> {
+                    dialog.cancel();
+                });
+
+        return builder.create();
+    }
+
+}
---
+++ app/src/main/java/com/github/ue_museumfilter/MainActivity.java
@@ -0,0 +1,67 @@
+package com.github.ue_museumfilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.ue_museumfilter.utils.APIRestService;
+import com.github.ue_museumfilter.utils.RetrofitClient;
+import com.github.ue_museumfilter.utils.data.MuseumRes;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity implements DialogFilter.OnFilterSelectedListener {
+
+    Button btn_open_dialog;
+    Button btn_aplicar_filtro;
+    String distrito;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_open_dialog = findViewById(R.id.btn_buscar);
+        btn_aplicar_filtro = findViewById(R.id.btn_aplicar_filtro);
+
+        btn_open_dialog.setOnClickListener(v -> {
+            DialogFilter dialogFilter = new DialogFilter();
+            dialogFilter.show(getSupportFragmentManager(), "dialogFilter");
+        });
+
+        btn_aplicar_filtro.setOnClickListener(v -> {
+            Retrofit retrofit = RetrofitClient.getClient(APIRestService.BASE_URL);
+            APIRestService apiRestService = retrofit.create(APIRestService.class);
+            Call<MuseumRes> call = apiRestService.getMuseums(distrito);
+
+            call.enqueue(new retrofit2.Callback<MuseumRes>() {
+                @Override
+                public void onResponse(Call<MuseumRes> call, Response<MuseumRes> response) {
+                    if (response.isSuccessful()) {
+                        MuseumRes museumRes = response.body();
+                        for (int i = 0; i < museumRes.getMuseums().size(); i++) {
+                            System.out.println(museumRes.getMuseums().get(i).getTitle());
+                        }
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<MuseumRes> call, Throwable t) {
+                    Toast.makeText(MainActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+        });
+        });
+    }
+
+    @Override
+    public void onFilterSelected(String distrito) {
+        this.distrito = distrito;
+        System.out.println("Filter text: " + distrito);
+    }
+}
